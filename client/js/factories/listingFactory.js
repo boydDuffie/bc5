87897495033f644ca5e3e2fc29968b7f069f6f05
boydDuffie <shmeeves@ufl.edu>
2019-02-11 22:00:42 -0500

    think this might be one step away woo

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,16 +1,16 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('/api/listings');
+      return $http.get('https:///api/listings');
     },
   
   create: function(listing) {
-    return $http.post('/api/listings', listing);
+    return $http.post('https:///api/listings', listing);
     }, 
 
     /*Return result of HTTP delete method*/
     delete: function(id) {
-      return $http.delete('/api/listings/' + id);
+      return $http.delete('https:///api/listings/' + id);
     }
   };
 
