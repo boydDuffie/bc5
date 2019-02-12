9a3993a0abdc19bf36dd80bf3a199455310f16cf
boydDuffie <shmeeves@ufl.edu>
2019-02-11 21:10:57 -0500

    maybe this?

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@
 module.exports = {
   db: {
     uri: 'mongodb://user:onetwo3@ds131905.mlab.com:31905/bc4db', //place the URI of your mongo database here.
-  },
+  }
 };
 
 /* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() */
