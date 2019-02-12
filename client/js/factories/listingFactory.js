8459118eda05aca46c92bf28d170dc19bbe78ed1
boydDuffie <shmeeves@ufl.edu>
2019-02-11 22:24:45 -0500

    holy cow this is taking forever

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,16 +1,16 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://murmuring-bastion-62203.herokuapp.com/api/listings');
+      return $http.get('https://pure-basin-39576.herokuapp.com/api/listings');
     },
   
   create: function(listing) {
-    return $http.post('https://murmuring-bastion-62203.herokuapp.com/api/listings', listing);
+    return $http.post('https://pure-basin-39576.herokuapp.com/api/listings', listing);
     }, 
 
     /*Return result of HTTP delete method*/
     delete: function(id) {
-      return $http.delete('https://murmuring-bastion-62203.herokuapp.com/api/listings' + id);
+      return $http.delete('https://pure-basin-39576.herokuapp.com/api/listings' + id);
     }
   };
 
